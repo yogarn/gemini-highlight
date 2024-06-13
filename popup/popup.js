079832c95e0938d27041537184795108a85ec1d1
Yoga Raditya <radityanalaa@gmail.com>
2024-06-13 20:09:37 +0700

    chore: Add support for firefox

--- popup/popup.js
+++ popup/popup.js
@@ -1,24 +1,23 @@
 function setAPIKey(apiKey) {
-    chrome.tabs.query({ active: true, currentWindow: true })
-        .then((tabs) => {
-            chrome.tabs.sendMessage(tabs[0].id, {
-                action: "setAPIKey",
-                apiKey: apiKey,
-            });
-        })
-        .then(() => {
-            alert("API key set successfully.");
-        })
-        .catch(reportError);
+  browser.tabs
+    .query({ active: true, currentWindow: true })
+    .then((tabs) => {
+      browser.tabs.sendMessage(tabs[0].id, {
+        action: "setAPIKey",
+        apiKey: apiKey,
+      });
+    })
+    .then(() => {})
+    .catch(reportError);
 }
 
 document.addEventListener("click", (e) => {
-    if (e.target.id === "enable-api-key") {
-        const apiKey = document.getElementById("api-key-input").value;
-        if (apiKey) {
-            setAPIKey(apiKey);
-        } else {
-            setAPIKey("disable");
-        }
+  if (e.target.id === "enable-api-key") {
+    const apiKey = document.getElementById("api-key-input").value;
+    if (apiKey) {
+      setAPIKey(apiKey);
+    } else {
+      setAPIKey("disable");
     }
+  }
 });
